820a7bd7e997b684403daf5d4905c6a4ca4381ae
GUBONWOO <zcwxzsx@gmail.com>
2023-09-14 16:15:04 +0900

    react router

---
+++ basic4/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Videos from './pages/Videos';
+import Root from './pages/Root';
+import VideoDetail from './pages/VideoDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/videos', element: <Videos /> },
+      { path: '/videos/:videosId', element: <VideoDetail /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
