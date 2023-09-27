39ee14946b9124fc1b14e4fb535c0c979cb64e99
GUBONWOO <zcwxzsx@gmail.com>
2023-09-27 11:32:36 +0900

    shoppy pj2

--- basic7/src/components/Navbar.jsx
+++ basic7/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
+import { login, logout, onUserStateChange } from '../api/firebase';
+import User from '../components/User';
+
 export default function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -10,12 +19,14 @@ export default function Navbar() {
         <h1>Shoppy</h1>
       </Link>
       <nav className='flex items-center gap-4 font-semibold'>
-        <Link to='/product'>Products</Link>
+        <Link to='/products'>Products</Link>
         <Link to='/carts'>Carts</Link>
         <Link to='/products/new' className='text-2xl'>
           <BsFillPencilFill />
         </Link>
-        <button>Login</button>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
